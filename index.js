3f327252e80335e9d024f327c1fefe27f6c67187
Ivylucyawuor <awuorivy09>
2024-07-14 11:05:30 +0300

    dodger game events

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+//Move dodger to the left
+function moveDodgerLeft() {
+   const dodger = document.getElementById('dodger');
+   const leftNumbers = dodger.style.left.replace('px','');
+   let left = parseInt(leftNumbers, 10);
+   
+   if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+   }
+}
+// move dodger right
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) { // assuming the dodger's container width is 400px and dodger's width is 40px
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
